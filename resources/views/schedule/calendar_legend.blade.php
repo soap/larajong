bf7c792a7c40dc39c82f78f32f3d66cf70dbdb63
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2023-06-25 22:59:38 +0700

    Prepare schedule view

---
+++ resources/views/schedule/calendar_legend.blade.php
@@ -0,0 +1,10 @@
+<div id="legend-container">
+    <div class="legend reservable">{{__('jongman.reservable') }}</div>
+	<div class="legend unreservable">{{ __('jongman.unreservable') }}</div>
+	<div class="legend reserved">{{ __('jongman.reserved') }}</div>
+	<div class="legend reserved mine">{{__('jongman.my_reservation') }}</div>
+	<div class="legend reserved participating">{{ __('jongman.participating') }}</div>
+	<div class="legend reserved pending">{{__('jongman.pending')}}</div>
+	<div class="legend pasttime">{{ __('jongman.past') }}</div>
+	<div class="legend restricted">{{ __('jongman.restricted') }}</div>	
+</div>
---
+++ resources/views/schedule/calendar_main.blade.php
@@ -0,0 +1,25 @@
+<div id="reservations">
+    <!-- start of one day reservation table -->
+    @foreach($displayDates as $date)
+    <table class="reservations">
+        <tr class="">
+            <td class="resdate">{{ $date->format($dailyDateFormat) }}</td>
+            @foreach($layout->getPeriods($date, true) as $period) 
+            <td class="reslabel" colspan="{{ $period->span() }}">{{ $period->label($date)}}</td>
+            @endforeach
+        </tr>
+    @foreach($resources as $resource)
+    @php $slots = $layout->getLayout($date, $resource->id) @endphp
+        <tr class="slots">
+            <td class="resourcename">
+            </td>
+        
+        @foreach($slots as $slot)
+            <!-- call SlotFactory::display($slot, $slotRef, true, $this);
+        @endforeach
+        </tr>
+    @endforeach
+    </table>
+    @endforeach
+    <!-- end of one day reservation table -->
+</div>
